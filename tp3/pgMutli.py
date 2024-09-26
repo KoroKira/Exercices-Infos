961e9b8db1b32d6a6cde734ac06ea95b0e798f4d
Lucafr15 <151518317+Lucafr15@users.noreply.github.com>
2024-09-26 11:53:34 +0200

    Continuation du exo

--- tp3/pgMutli.py
+++ tp3/pgMutli.py
@@ -4,7 +4,8 @@ def multi(nbr1, nbr2):
     return nbr1 * nbr2
 
 # test
-m=multi(3,2.2)
-print(m) # 6.6
-S=somme(3,2.2)
-print(S)
+if __name__ =="__main__":
+    m=multi(3,2.2)
+    print(m) # 6.6
+    S=somme(3,2.2)
+    print(S)
--- tp3/pgSomme.py
+++ tp3/pgSomme.py
@@ -2,5 +2,6 @@ def somme(nb1, nb2):
     return(nb1 + nb2)
 
 # test
-s=somme(5,6.2)
-print(s) # 11.2
+if __name__ =="__main__":
+    s=somme(5,6.2)
+    print(s) # 11.2

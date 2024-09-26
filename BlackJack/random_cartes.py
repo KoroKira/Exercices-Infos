af9e0a31b13a694c4edc44691987b52383e05e67
Guilhem Desarcy-Lemiere <guilhem.desarcy-lemiere@2027.icam.fr>
2024-09-26 11:08:34 +0200

    Update random_cartes.py

--- BlackJack/random_cartes.py
+++ BlackJack/random_cartes.py
@@ -1,11 +1,11 @@
 import random
-from cartes import print_card
+from cartes import print_card # ca appelle la fonction print_card du fichier cartes.py
 
 def main():
     rangs = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
     suites = ["♠", "♥", "♦", "♣"]
 
-    for _ in range(2):  
+    for _ in range(2):  # modifier 2, ca permet de générer un nombre de cartes précises
         rang = random.choice(rangs)
         suite = random.choice(suites)
         print_card(rang, suite)

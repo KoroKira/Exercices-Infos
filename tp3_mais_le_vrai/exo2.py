73de4cdd88eca674fde954f4d0037cf98b715ad5
Guilhem Desarcy-Lemiere <guilhem.desarcy-lemiere@2027.icam.fr>
2024-10-29 15:26:19 +0100

    push+

---
+++ tp3_mais_le_vrai/exo2.py
@@ -0,0 +1,7 @@
+# Affichage de la phrase à partir de la liste des mots :
+
+ch = ['se', 'soigner', 'est', 'un', 'devoir', 'moral']
+for mot in ch:
+    print(mot, end=" ")
+print()  # Pour un saut de ligne à la fin
+
